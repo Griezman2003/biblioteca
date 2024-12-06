2415345fc4042a58e28ebaf7becdc19d4a37298d
Griezman2003 <garciagomezgamaliel@gmail.com>
2024-12-06 11:06:26 -0600

    implementacion de las rutas 50%  terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent,  RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl:'./app.component.css'
 })
 
 export class AppComponent {
   title = 'Biblioteca';
+  constructor(private router: Router) {}
+  openInformacionPage(): void {
+  window.open('/Informacion', '_blank');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {InformacionComponent} from './informacion/informacion.component';
+import { AppComponent } from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'Informacion', component: InformacionComponent },
+     // { path: 'Header', component: HeaderComponent },
+];
