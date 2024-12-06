0d58daf855a820dbd794b9c42b341ed76b77c43b
Griezman2003 <garciagomezgamaliel@gmail.com>
2024-12-06 13:36:22 -0600

    consumir servicio de json en la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { Router } from '@angular/router';
+import { JsonService } from './service/json.service'; 
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,  RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HeaderComponent,  RouterLink, RouterLinkActive ],
   templateUrl: './app.component.html',
   styleUrl:'./app.component.css'
 })
 
 export class AppComponent {
   title = 'Biblioteca';
-  constructor(private router: Router) {}
+  
+  books: any[] = [];
+
+  constructor(
+    private router: Router, 
+    private jsonService: JsonService
+  ) {}
+
+
   openInformacionPage(): void {
-  window.open('/Informacion', '_blank');
+    window.open('/Libro', '_blank'); 
+  }
+
+  ngOnInit(): void {
+    this.jsonService.getBook().subscribe(
+      (data) => {
+        this.books = data;
+      },
+      (error) => {
+        console.error('Error fetching books:', error);
+      }
+    );
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
-import {InformacionComponent} from './informacion/informacion.component';
+import {LibroComponent} from './libro/libro.component';
 import { AppComponent } from "./app.component";
 
 export const routes: Routes = [
     { path: '', component: AppComponent },
-    { path: 'Informacion', component: InformacionComponent },
+    { path: 'Libro', component: LibroComponent },
      // { path: 'Header', component: HeaderComponent },
 ];
+
