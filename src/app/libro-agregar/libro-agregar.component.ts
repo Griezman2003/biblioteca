4d28f5cbe4d6a3cb7561800a4381519dc5fa9c40
KGGJesus <jesusbalan1232@gmail.com>
2024-12-09 13:49:15 -0600

    form de agregar libros

--- src/app/libro-agregar/libro-agregar.component.ts
+++ src/app/libro-agregar/libro-agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-libro-agregar',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './libro-agregar.component.css'
 })
 export class LibroAgregarComponent {
+  libro = { titulo: '', autor: '', genero: '', anio_publicacion: null };
 
+  constructor(private http: HttpClient) {}
+
+  agregarLibro() {
+    this.http.post('http://localhost:3000/libros', this.libro)
+      .subscribe(() => {
+        alert('Libro agregado correctamente');
+        this.libro = { titulo: '', autor: '', genero: '', anio_publicacion: null };
+      });
+  }
 }
