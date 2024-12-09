13c9029a740010f5414203e1bad89be9b0c2451d
Griezman2003 <garciagomezgamaliel@gmail.com>
2024-12-09 13:18:59 -0600

    nueva ruta creada para la vista subir archivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {LibroComponent} from './libro/libro.component';
 import {LibroDetalleComponent} from './libro-detalle/libro-detalle.component'
+import { LibroAgregarComponent } from "./libro-agregar/libro-agregar.component";
 
 export const routes: Routes = [
     { path: '',component: LibroComponent},
     {path: 'libro-detalle/:titulo', component: LibroDetalleComponent},
+    {path: 'libro-agregar', component: LibroAgregarComponent},
     { path: '**', redirectTo: '' },
 ];
 
---
+++ src/app/libro-agregar/libro-agregar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libro-agregar',
+  imports: [],
+  templateUrl: './libro-agregar.component.html',
+  styleUrl: './libro-agregar.component.css'
+})
+export class LibroAgregarComponent {
+
+}
